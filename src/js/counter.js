358913b74afcf69713c73304fc1b5d82bba8dbe0
Artem Kiselev <artem.ki@devcaz.com>
2017-07-17 18:14:55 +0300

    add multiodometers

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,123 +1,4 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", (ev) => {
-	(()=>{
-
-if (!Object.prototype.watch) {
-  Object.defineProperty(Object.prototype, "watch", {
-    enumerable: false
-    , configurable: true
-    , writable: false
-    , value: function (prop, handler) {
-      var
-      oldval = this[prop]
-      , newval = oldval
-      , getter = function () {
-        return newval;
-      }
-      , setter = function (val) {
-        oldval = newval;
-        return newval = handler.call(this, prop, oldval, val);
-      }
-      ;
-
-      if (delete this[prop]) { // can't watch constants
-        Object.defineProperty(this, prop, {
-          get: getter
-          , set: setter
-          , enumerable: true
-          , configurable: true
-        });
-      }
-    }
-  });
-}
-
-// object.unwatch
-if (!Object.prototype.unwatch) {
-  Object.defineProperty(Object.prototype, "unwatch", {
-    enumerable: false
-    , configurable: true
-    , writable: false
-    , value: function (prop) {
-      var val = this[prop];
-      delete this[prop]; // remove accessors
-      this[prop] = val;
-    }
-  });
-}
-
-		let a = new JackPotCounter({
-			tickLength: 5000,
-			qtyPerTick: 10,
-			value: 0
-		});
-		
-		let output = document.getElementById('output'),
-			play = document.getElementById('tickPlay'),
-			stop = document.getElementById('tickStop'),
-			submit = document.getElementById('submitData'),
-			submitTick = document.getElementById('submitTick'),
-			random = document.getElementById('random'),
-			inputRandom = document.getElementById('inputRandom'),
-			log = document.getElementById('log'),
-			input = document.getElementById('dataVal'),
-			inputTick = document.getElementById('tickVal');			
-	
-		a.elemArr = Array.from(document.querySelectorAll('.roller'));
-		a.random = random.checked;
-		a.max = +document.getElementById('inputRandom').value;
-		
-		a.watch("currentValue", function (id, oldval, newval) {
-    		return newval;
-		});
-
-		play.onclick= (ev) => {
-			ev.preventDefault();
-			a.random = random.checked;
-			a.max = +document.getElementById('inputRandom').value;
-				//a.tickPlay().echo(output);
-			a.generator();
-		};
-
-		stop.onclick= (ev) => {
-			ev.preventDefault();
-			a.tickStop().echoStop();
-			a.unwatch("currentValue");			
-		};
-
-		/*setCurrentValue*/
-		submit.onclick = (ev) => {
-			ev.preventDefault();
-			a.random = random.checked;
-			a.max = +document.getElementById('inputRandom').value;
-			a.setCurrentValue(input.value);
-			a.save(a.buffer);
-			a.run().echo(output);
-		};
-		input.onkeydown = (ev) => {
-			if(ev.keyCode == 13){
-				a.setCurrentValue(input.value);
-				a.run().echo(output);
-			}
-		};
-		/*setTickLength*/
-		submitTick.onclick = (ev) => {
-			ev.preventDefault();
-			a.setTickLength(inputTick.value);
-		};
-		inputTick.onkeydown = (ev) => {
-			if(ev.keyCode == 13){
-				a.setTickLength(inputTick.value);
-				a.getLog("\n⮡ " + a.tickLength + "ms 🢣 default interval 🢣");
-			}
-		};
-		
-console.log(a);
-	})();
-}, false);
-
-
-
 function JackPotCounter(options){
 	let that = this;
 	const ANGLE = 36;
@@ -142,7 +23,9 @@ function JackPotCounter(options){
 	this.currentValue = options&&options.value ? options.value : 0;
 	this.ticker = [];
 	this.echoTicker = [];
-	this.elemArr = [];
+	this.jackName = options&&options.jackName ? options.jackName : 'new game';
+	this.numRolls = options&&options.numRolls ? options.numRolls : 7;
+	this.elemArr = options&&options.elemArr ? options.elemArr : [];
 	this.tickLength = options&&options.tickLength ? options.tickLength : 1000;
 	this.qtyPerTick = options&&options.qtyPerTick ? options.qtyPerTick : 1;
 
@@ -155,84 +38,24 @@ function JackPotCounter(options){
 				this.data.diffBetweenLatests = +(this.buffer[0]-this.buffer[1]).toFixed(2);
 				/*-*/this.data.time = Date.now();
 				this.data.arrayStamp = this.buffer.join(', ');
-				/*emu*/ if(this.random) this.currentValue = this.currentValue + randomInteger(0, this.max);
-				this.dev();
 			}, this.tickLength);
 
 		};
 		return this;
 	};
 
-	this.generator = () => {
-		if(this.ticker.length < 1 && this.random) {
-			this.ticker[0] = setInterval(()=>{
-				this.currentValue += randomInteger(0, this.max);
-				this.transform(this.elemArr);
-				this.dev();
-			}, this.tickLength + 2000);
-		}
-		else that.run();		
-	}
-
-	this.run = () => {
+	this.run = (ev) => {
+		ev.preventDefault();
 		this.transform(this.elemArr);
-		this.dev();
 		this.buffer.splice(0, 0, this.currentValue);
 		this.buffer.length = 10;
-		console.log(this.buffer);
 		return this;
 	};
 
-	this.dev = () => {
-		//console.log(this.coordinates);
-		this.getLog("\n⮡ " + this.currentValue + " 🢣🢣🢣🢣🢣🢣🢣 random: " + this.random + " 🢣 max: " + this.max);
-		this.getLog("time: " + this.coordinates.time);
-		this.getLog("rotate: " + this.coordinates.rotate);
-		this.getLog("speed: " + this.coordinates.speed);
-		this.getLog("speedAverage: " + this.coordinates.speedAverage);
-		this.getLog("deviation: " + this.coordinates.deviation);
-		this.getLog("duration: " + this.coordinates.duration);
-		this.getLog("delay: " + this.coordinates.delay);
-	    return this;
-	};
-	this.echo = ($) => {
-		this.echoTicker[0] = setInterval(()=>{
-			$.innerHTML = that.currentValue;
-		},this.tickLength);
-		return this;
-	};
-	this.echoStop = () => {
-		clearInterval(this.echoTicker);
-		this.echoTicker = [];
-		return this;
-	};
-	this.tickStop = () => {
-		clearInterval(this.ticker);
-		this.ticker = [];
-		return this;
-	};
-	this.getCurrentValue = () => {
-		return this.currentValue;
-	};
-
-	this.setCurrentValue = (value) => {
-		if(isNaN(value/2)) 
-			return that.getLog('= waiting for integer =');
-		return this.currentValue = value;
-	};
-
-	this.setTickLength = (value) => {
-		if(isNaN(value/2)) 
-			return that.getLog('= waiting for integer =');
-		return this.tickLength = value;
-	};
-
-	this.setRandom = (value) => {
-		return this.currentValue = this.currentValue + randomInteger(0, value);
-	};
-
-	this.getDiff = () => {
-		return this.data.diffBetweenLatests;
+	this.setCurrentValue = (v) => {
+		if(isNaN(v/2)) 
+			return console.info('= waiting for integer =');
+		return this.currentValue = v;
 	};
 
 	this.controller = (rotate) => {
@@ -253,7 +76,7 @@ function JackPotCounter(options){
 	this.transform = (elemArr) => {
 		var arr = elemArr.slice(),
 			data = that.currentValue * 100;
-		for(var i = 7; i >= 0; i--) {
+		for(var i = arr.length; i >= 0; i--) {
 			var multiplier = parseInt(data / Math.pow(10, 7 - i)),
 				rotate = multiplier * ANGLE;
 			if(i == 7) {that.controller(rotate);}
@@ -277,58 +100,77 @@ function JackPotCounter(options){
 		el.setAttribute('style', "transform: rotateX(" + rotate  + "deg);transition-duration:" + duration + "ms; transition-delay:" + delay + "ms;transition-timing-function:" + bezier);
 	};
 
-	this.getLog = (msg) => {
-		var ot = log.scrollHeight - log.clientHeight;
-		if (ot > 0) log.scrollTop = ot;
-		return log.textContent += msg + "\n";
-	};
+	(this.drawOdometer = () => {
+		let parent = document.querySelector('.center');
+			let jackpotItem = document.createElement('div');
+				jackpotItem.className = 'jackpot-item';
+				jackpotItem.id = that.jackName;
+				jackpotItem.innerHTML = "<div class='jackpot-name'>" + that.jackName + "</div>";
+			
+			let jackpotCounter = document.createElement('div');
+			jackpotCounter.className = 'jackpot-counter';
 
-	function randomInteger(min, max) {
-	    let rand = min - 0.5 + Math.random() * (max - min + 1)
-	    rand = Math.round(rand);
-	    return rand;
-	};
+
+		let drawingCells = "<div class='jackpot-counter-cell'><div class='roller' data-rotate='0' data-duration='5000' style='transform: rotateX(0deg);transition-duration:5000ms'><div class='plane figure0'>0</div><div class='plane figure1'>1</div><div class='plane figure2'>2</div><div class='plane figure3'>3</div><div class='plane figure4'>4</div><div class='plane figure5'>5</div><div class='plane figure6'>6</div><div class='plane figure7'>7</div><div class='plane figure8'>8</div><div class='plane figure9'>9</div></div></div>";
+		let wrapper = document.createElement('div');
+		wrapper.className = 'jackpot-counter-wrapper';
+		let collection = '';
+		for (let i = that.numRolls; i > 0; i--) collection += drawingCells;
+		wrapper.innerHTML=collection;
+		jackpotCounter.appendChild(wrapper);
+		jackpotItem.appendChild(jackpotCounter);
+		parent.appendChild(jackpotItem);
+		that.elemArr = Array.from(document.querySelectorAll('#' + that.jackName + ' .roller'));
+	})();
+
+	/*watch polyfill*/
+	if (!Object.prototype.watch) {
+  	Object.defineProperty(Object.prototype, "watch", {
+    enumerable: false
+    , configurable: true
+    , writable: false
+    , value: function (prop, handler) {
+      var
+      oldval = this[prop]
+      , newval = oldval
+      , getter = function () {
+        return newval;
+      }
+      , setter = function (val) {
+        oldval = newval;
+        return newval = handler.call(this, prop, oldval, val);
+      }
+      ;
+
+      if (delete this[prop]) { 
+        Object.defineProperty(this, prop, {
+          get: getter
+          , set: setter
+          , enumerable: true
+          , configurable: true
+        });
+      }
+    }
+  });
+}
+
+if (!Object.prototype.unwatch) {
+  Object.defineProperty(Object.prototype, "unwatch", {
+    enumerable: false
+    , configurable: true
+    , writable: false
+    , value: function (prop) {
+      var val = this[prop];
+      delete this[prop]; 
+      this[prop] = val;
+    }
+  });
+}
+
+this.watch("this.currentValue", function (id, oldval, newval) {
+    		return newval;
+		});
 
 	return this;
 };
-
-
-
-function getDecimal(num) {
-  var str = "" + num;
-  var zeroPos = str.indexOf(".");
-  if (zeroPos == -1) return 0;
-  str = str.slice(zeroPos);
-  return +str;
-};
-
-function rough(value, factor){
-	if(isNaN(value/2)) return false;
-
-	factor = factor || 1; 
-	let res = Math.random() >= .5 ? value + (Math.random()*factor) : value - (Math.random()*factor);
-
-	res = +(res).toFixed(2);
-	return res;
-};
-
-function createArray(arg) {
-	if(!arg)
-		arg = 0;
-	var str = "" + (arg).toFixed(2), string = "";
-  	for(var i = 0; i < str.length; i++){
-    	//if(i == 0 && +str[i] == 0) continue; 
-    	if(!isNaN(+str[i])) string += str[i];
-    }
-	arg = string.split("");
-	return arg;
-};
-
-
-function randomInteger(min, max) {
-    rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
-};
-
 
